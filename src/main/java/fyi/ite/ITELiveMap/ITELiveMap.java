3b81eec97735824d51b22bdffac96a8d36e60f67
ObsidianSeal <bmgosselin@gmail.com>
2025-12-05 22:53:50 -0500

    commit 0

---
+++ src/main/java/fyi/ite/ITELiveMap/ITELiveMap.java
@@ -0,0 +1,16 @@
+package fyi.ite.ITELiveMap;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ITELiveMap extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
